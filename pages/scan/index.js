f7c7b235cf7dd2c211fe3044abc19f2e2952e437
pfdiy <pfdiy@hotmail.com>
2022-03-24 01:27:45 +0800

    fix

--- pages/scan/index.js
+++ pages/scan/index.js
@@ -1,70 +1,94 @@
-let QQMapWx = require("../../utils/lbs/qqmap-wx-jssdk")
+let QQMapWx = require("../../utils/lbs/qqmap-wx-jssdk");
 let qqmapsdk;
 const app = getApp();
+
 // pages/scan/index.js
 Page({
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        value: "82",
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    value: "82",
+    latitude: "",
+    longitude: "",
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        qqmapsdk = new QQMapWx({
-            key: app.lbskey
-        })
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.getSetting({
+      success: (res) => {
+        this.getAddress();
+      },
+    });
+  },
 
+  getAddress() {
+    qqmapsdk = new QQMapWx({
+      key: app.lbskey,
+    });
+    let that = this;
+    wx.getLocation({
+      type: "gcj02",
+      isHighAccuracy: true,
+      success: (res) => {
+        let lat = res.latitude;
+        let lon = res.longitude;
+        qqmapsdk.reverseGeocoder({
+          location: { latitude: lat, longitude: lon },
+          success: (res) => {
+            let address = res.result.address;
+            console.log(address);
+            that.setData({
+              latitude: lat,
+              longitude: lon,
+            });
+          },
+        });
+      },
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
+  sanCode: () => {
+    wx.scanCode({
+      success(res) {
+        console.log(res);
+      },
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
 
-    onReady: function () {},
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-        wx.getLocation({
-            type: 'gcj02',
-            altitude:true,
-            isHighAccuracy:true,
-            success(res) {
-                const latitude = res.latitude
-                const longitude = res.longitude
-                console.log(latitude)
-                console.log(longitude)
-            },
-        })
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {},
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {},
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {},
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {},
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {},
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {},
 });
